b6f541e9f662acb837f041f4a561c7c644d4e8c4
duythanh246 <duyduy20303@gmail.com>
2024-11-22 02:30:31 +0700

    nodejs

---
+++ src/controllers/specialtyController.js
@@ -0,0 +1,37 @@
+import specialtyServive from '../services/specialtyService';
+
+let createSpecialty = async (req, res) => {
+    try {
+        let infor = await specialtyServive.createSpecialty(req.body);
+
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error server'
+        })
+    }
+}
+
+let getAllSpecialty = async (req, res) => {
+    try {
+        let infor = await specialtyServive.getAllSpecialty();
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error server'
+        })
+    }
+}
+
+module.exports = {
+    createSpecialty: createSpecialty,
+    getAllSpecialty: getAllSpecialty
+}
--- src/route/web.js
+++ src/route/web.js
@@ -3,6 +3,8 @@ import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController";
+import specialtyController from "../controllers/specialtyController";
+
 let router = express.Router();
 
 let initWebRouters = (app) => {
@@ -34,6 +36,9 @@ let initWebRouters = (app) => {
 
     router.post('/api/patient-book-appointment', patientController.postBookAppointment);
     router.post('/api/verify-book-appointment', patientController.postVerifyBookAppointment);
+
+    router.post('/api/create-new-specialty', specialtyController.createSpecialty)
+    router.get('/api/get-specialty', specialtyController.getAllSpecialty);
 
     return app.use("/", router);
 }
